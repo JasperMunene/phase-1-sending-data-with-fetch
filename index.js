ec794d027ff991c1767d4693b66f1466539455f7
JasperMunene <jaspermunene2022@gmail.com>
2024-10-10 11:58:17 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    
+    const body = {
+        name: name,
+        email: email
+    };
+    
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(body)
+    };
+
+    
+    return fetch(url, configurationObject)
+    .then(response => {
+        return response.json();  
+    })
+    .then(data => {
+        const bodyElement = document.querySelector('body');
+        const idElement = document.createElement('p');
+        idElement.innerHTML = data.id;  
+        bodyElement.append(idElement);
+    })
+    .catch(function(error) {
+        const bodyElement = document.querySelector('body');
+        const errorElement = document.createElement('p');
+        errorElement.innerHTML = 'Unauthorized Access'; 
+        bodyElement.append(errorElement)
+
+    });
+}
